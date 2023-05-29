db487c3c24532965a839d2d416c22d21cfa1dfba
MasakiFukunishi <fmsknn@gmail.com>
2023-05-29 09:26:51 -0700

    Add stack by linked list

---
+++ src/stack-linked-list.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return holdingPointer;
+  }
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push("A");
+myStack.push("B");
+myStack.push("C");
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
