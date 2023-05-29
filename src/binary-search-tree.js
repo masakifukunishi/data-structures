92c7d18292d182798abb336c0da2b64ddfb6254d
MasakiFukunishi <fmsknn@gmail.com>
2023-05-29 14:41:39 -0700

    Add lookup to binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -36,7 +36,20 @@ class BinarySearchTree {
     }
   }
   lookup(value) {
-    //Code here
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return null;
   }
 }
 
